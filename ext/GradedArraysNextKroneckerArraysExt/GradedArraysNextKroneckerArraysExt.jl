7eb821a603298db314ba9b8bd8769f5461ce2a53
mtfishman <mfishman@flatironinstitute.org>
2025-08-18 14:23:40 -0400

    Extensions and cleanup

--- ext/GradedArraysNextKroneckerArraysExt/GradedArraysNextKroneckerArraysExt.jl
+++ ext/GradedArraysNextKroneckerArraysExt/GradedArraysNextKroneckerArraysExt.jl
@@ -1,10 +1,15 @@
+module GradedArraysNextKroneckerArraysExt
+
+using GradedArraysNext: GradedArraysNext, dual
 using KroneckerArrays: CartesianProduct, ×, arg1, arg2
-function dual(a::CartesianProduct)
+function GradedArraysNext.dual(a::CartesianProduct)
   return dual(arg1(a)) × dual(arg2(a))
 end
 
 using KroneckerArrays:
   CartesianProductUnitRange, cartesianproduct, cartesianrange, unproduct
-function dual(a::CartesianProductUnitRange)
+function GradedArraysNext.dual(a::CartesianProductUnitRange)
   return cartesianrange(dual(cartesianproduct(a)), dual(unproduct(a)))
+end
+
 end
--- src/sector.jl
+++ src/sector.jl
@@ -1,5 +1,5 @@
 abstract type Sector{T} <: AbstractUnitRange{T} end
-Base.first(::Sector) = 1
+Base.first(s::Sector) = one(eltype(s))
 Base.last(s::Sector) = length(s)
 Base.axes(s::Sector) = (s,)
 Base.axes1(s::Sector) = s
@@ -12,44 +12,15 @@ end
 abstract type AbelianSector{T} <: Sector{T} end
 Base.length(::AbelianSector) = 1
 
-# TODO: Are these good definitions?
-using FillArrays: Eye
-function Base.similar(
-  A::Type{<:AbstractArray{T}}, ax::Tuple{AbelianSector,AbelianSector}
-) where {T}
-  return Eye{T}(ax)
-end
-using KroneckerArrays: Delta
+using DiagonalArrays: Delta
+
 function Base.similar(
   A::Type{<:AbstractArray{T}}, ax::Tuple{AbelianSector,Vararg{AbelianSector}}
 ) where {T}
   return Delta{T}(ax)
 end
 
-using DiagonalArrays: DiagonalArray
-using FillArrays: Eye, OnesVector
-using KroneckerArrays: Delta
-using TensorAlgebra: TensorAlgebra, BlockedTuple, ReshapeFusion
-# TODO: Define and use `Delta`.
-# TODO: Generalize beyond `AbelianSector`, for example `ProductSector`.
-function TensorAlgebra.unmatricize(
-  ::ReshapeFusion,
-  a::Eye,
-  ax::BlockedTuple{2,<:Any,<:Tuple{AbelianSector,Vararg{AbelianSector}}},
-)
+function Base.reshape(a::Delta, ax::Tuple{AbelianSector,Vararg{AbelianSector}})
   length(a) == prod(length, ax) || throw(DimensionMismatch("reshape sizes don't match"))
-  return Delta{eltype(a)}(Tuple(ax))
+  return Delta{eltype(a)}(ax)
 end
-
-## function Base.reshape(
-##   a::DiagonalArray{T,<:Any,<:OnesVector{T}}, ax::Tuple{AbelianSector,AbelianSector}
-## ) where {T}
-##   length(a) == prod(length, ax) || throw(DimensionMismatch("reshape sizes don't match"))
-##   return Eye{T}(ax)
-## end
-## function Base.reshape(
-##   a::Eye{T}, ax::Tuple{AbelianSector,AbelianSector}
-## ) where {T}
-##   length(a) == prod(length, ax) || throw(DimensionMismatch("reshape sizes don't match"))
-##   return Eye{T}(ax)
-## end
